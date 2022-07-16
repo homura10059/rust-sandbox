8b2e83fe8f0adabc0fd31b8c815939acfc6b8eee
homura <preasper0+github@gmail.com>
2022-07-16 13:29:22 +0900

    add sam sample

--- aws-lambda-sample/src/main.rs
+++ aws-lambda-sample/src/main.rs
@@ -1,20 +1,14 @@
-use lambda_http::{run, service_fn, Error, IntoResponse, Request, RequestExt, Response};
+use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/lambda-http/examples
-async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/lambda-runtime/examples
+/// - https://github.com/aws-samples/serverless-rust-demo/
+async fn function_handler(event: LambdaEvent<CloudWatchEvent>) -> Result<(), Error> {
     // Extract some useful information from the request
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body("Hello AWS Lambda HTTP request")
-        .map_err(Box::new)?;
-    Ok(resp)
+    println!("{}", event.payload.detail.unwrap());
+    Ok(())
 }
 
 #[tokio::main]
